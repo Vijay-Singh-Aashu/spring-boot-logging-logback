fdd006f0ab030401958bcaa29564d4cad73d2788
Vijay Singh <vijaysinghaashu@gmail.com>
2023-11-24 10:55:52 +0530

    spring boot logging

---
+++ src/main/java/com/excellence/springbootlogginglogback/SpringBootLoggingLogbackApplication.java
@@ -0,0 +1,13 @@
+package com.excellence.springbootlogginglogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoggingLogbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLoggingLogbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence/springbootlogginglogback/SpringBootLoggingLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.springbootlogginglogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggingLogbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
